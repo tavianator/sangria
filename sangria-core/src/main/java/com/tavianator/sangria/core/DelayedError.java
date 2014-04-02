00d80a2b4414d2930e90df39d83d8887b13f6ae5
Tavian Barnes <tavianator@tavianator.com>
2014-04-02 16:43:47 -0400

    Re-license under the Apache 2.0 License.

--- sangria-core/src/main/java/com/tavianator/sangria/core/DelayedError.java
+++ sangria-core/src/main/java/com/tavianator/sangria/core/DelayedError.java
@@ -1,13 +1,19 @@
-/*********************************************************************
- * Sangria                                                           *
- * Copyright (C) 2014 Tavian Barnes <tavianator@tavianator.com>      *
- *                                                                   *
- * This library is free software. It comes without any warranty, to  *
- * the extent permitted by applicable law. You can redistribute it   *
- * and/or modify it under the terms of the Do What The Fuck You Want *
- * To Public License, Version 2, as published by Sam Hocevar. See    *
- * the COPYING file or http://www.wtfpl.net/ for more details.       *
- *********************************************************************/
+/****************************************************************************
+ * Sangria                                                                  *
+ * Copyright (C) 2014 Tavian Barnes <tavianator@tavianator.com>             *
+ *                                                                          *
+ * Licensed under the Apache License, Version 2.0 (the "License");          *
+ * you may not use this file except in compliance with the License.         *
+ * You may obtain a copy of the License at                                  *
+ *                                                                          *
+ * http://www.apache.org/licenses/LICENSE-2.0                               *
+ *                                                                          *
+ * Unless required by applicable law or agreed to in writing, software      *
+ * distributed under the License is distributed on an "AS IS" BASIS,        *
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. *
+ * See the License for the specific language governing permissions and      *
+ * limitations under the License.                                           *
+ ****************************************************************************/
 
 package com.tavianator.sangria.core;
 
